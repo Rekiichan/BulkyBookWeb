ba827b0ffb23ff9a358c51a52e01c8c555c46b44
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-10-07 13:49:53 +0700

    add category controller

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,7 +7,6 @@ namespace BulkyBookWeb.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
         }
         public DbSet<Category> Categories { get; set; }
     }
