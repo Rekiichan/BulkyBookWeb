ebaba12c2f863c63a9da3db5657cff64862a8d05
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-10-12 19:45:56 +0700

    up1210

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -17,5 +17,71 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        //CREATE
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(obj);
+            }
+            for (int i = 0; i < obj.Name.Length; i++)
+            {
+                if (obj.Name[i] > 'Z' || obj.Name[i] < 'A')
+                {
+                    if (obj.Name[i] < 'a' || obj.Name[i] > 'z')
+                    {
+                        ModelState.AddModelError("Name", "The characters is invalid, please enter only alphabet.");
+                        break;
+                    }
+                }
+            }
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //EDIT
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var CategoryFromDb = _db.Categories.Find(id);
+            //var CategoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            //var CategoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+
+
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
